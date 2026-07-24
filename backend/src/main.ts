f5f2b79105c7b0840824fd748faad9c7a807f64d
rakib3719 <bannah76769@gmail.com>
2026-07-24 19:45:48 +0600

    feat: integrate Swagger module for API documentation and endpoint exploration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { env } from './common/config/env.config';
 import cookieParser from 'cookie-parser';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,8 +15,20 @@ async function bootstrap() {
     credentials: true,
   });
 
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('Investra API')
+    .setDescription('The Investra API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .addCookieAuth('accessToken')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(env.PORT);
   console.log(`🚀 Server is running on: http://localhost:${env.PORT}`);
+  console.log(`📚 Swagger API Docs: http://localhost:${env.PORT}/api/docs`);
 }
 bootstrap();
 
